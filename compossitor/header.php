2dc4d188e536f9b6c1e510cd2d468232f97bd4e8
WonderSaves <igalax114@gmail.com>
2021-02-16 18:50:44 +0100

    Se crean las paginas y el navbar, asi como un poco de css

---
+++ compossitor/header.php
@@ -0,0 +1,5 @@
+  <head>
+    <meta charset="utf-8">
+    <title>Valiant</title>
+    <link rel="stylesheet" href="css/css.css">
+  </head>
---
+++ compossitor/navbar.php
@@ -0,0 +1,8 @@
+<ul class="navbar">
+  <li class="li"> <a class="home" href="#">Valiant</a>
+  </li>
+  <li class="li"> <a class="a" href="#">Historia</a>
+  </li>
+  <li class="li"> <a class="a" href="#">Personajes</a>
+  </li>
+</ul>
