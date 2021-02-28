191b11bae9483ac724544dbc51cf258572ba2df2
Isaac Casado <icm061202@gmail.com>
2021-02-28 21:54:10 +0100

    Añadida información en spanish

--- languages/spanish.php
+++ languages/spanish.php
@@ -7,4 +7,47 @@ $developers = "Desarrolladores";
 $user = "Usuario";
 $passwd = "Contraseña";
 $passwd2 = " Confirmar contraseña";
+$historyTextP1 = "Durante siglos, los humanos y los elfos esclavizaron a los orcos debido a su fuerza,
+capacidad de no enfermar, y su resistencia. Estos, eran tratados como lo peor, se les
+daba los trabajos mas duros, como por ejemplo la construcción de muros, en la que
+tenían que trabajar junto a los cadáveres de sus compañeros, muertos por el
+cansancio. La rebelión no tardó en llevarse a cabo cuando Urrek, un orco con una
+inteligencia superior a la media decidió que era hora de acabar con la represión de los
+orcos que les llevaba atormentando desde el día de su nacimiento. Estos se levantaron
+en masa en las grandes ciudades tomando el control por sorpresa de forma brutal e
+implacable ante la sorpresa de los elfos y humanos que no supieron como reaccionar.
+Tras estos sucesos Urrek no tardó en obsesionarse con el Orbe de Hael por lo que
+decidió el asedio de Dalingas.";
+$historyTextP2 = "Valiant, un niño mas en la ciudad de Dalingas nacido en el núcleo de un suburbio
+extremadamente conflictivo, entrenaba sin descanso para llegar a ser un Protector de
+Ithril, uno de los puestos mas importantes en la ciudad. La valiosa labor de estos
+protectores era cuidar el Orbe de Hael, uno de los artefactos mas poderosos
+concebidos en el Reino de Edelaiah. Tras la invasión de la ciudad por parte de los
+orcos, el líder del asalto Urrek, traicionando la voluntad de sus camaradas se hizo con
+el poder del Orbe y los sometió. Durante la invasión, la ciudad fue totalmente
+arrasada dejando a unos pocos supervivientes que consiguieron escapar antes de que
+los orcos penetrasen los muros de la ciudad. En el asedio la madre de Valiant fue
+ejecutada sin piedad por Urrek, ante la atenta mirada desesperada de su hijo. Tras
+escapar, Valiant se juró a si mismo que lucharía contra los orcos hasta su último
+aliento, desencadenando el inicio de su aventura hacia la venganza y la conquista de
+la antigua Dalingas, actualmente conocida como Ogkrum...";
+$historyTextP3 = "En un mundo ya casi conquistado por los orcos, las últimas resistencias de elfos y
+humanos se aliaron para combatir a las fuerzas ofensivas que atacaban
+constantemente sus últimos bastiones, como por ejemplo la ciudad de Xitra, la última
+ciudad de elfos y humanos. Esta, ubicada en el interior de una cueva era
+especialmente difícil de atacar debido a que la única entrada a la ciudad estaba
+custodiada por un dragón de Xare, una de las razas mas inteligentes de dragones, este
+estaba entrenado específicamente para atacar a orcos cuando le diesen la señal.
+Debido a la inteligencia de dicho dragón el Orbe no podía dominarle por lo que Urrek
+decidió no atacar a la ciudad por el momento.";
+$charactersTextValiant = "Valiant: Un joven que estaba decido a triunfar en la vida. Su seño se
+vió truncado por la invasión de su ciudad nativa, Dalingas a manos de un ejercito de orcos cuando
+el todavía era un niño, ante su impotencia de ver a su madre ser ejecutada por el orco que dirigía
+el asalto juró venganza y muerte a todos los que se interpusiesen en su camino";
+$charactersTextGladios = "Gladios: Uno de los mercenarios mas temidos por los orcos, ya no
+solo porque se dedica a cazarlos si no porque era un asesino letal y despiadado. Debido a su destreza
+y su necesidad de dinero se convirtió en un sanguinario mercenario, que algunos humanos contrataban para 
+'saldar' sus deudas con algunos orcos.";
+$charactersText
+$indexText = " ";
 ?>
